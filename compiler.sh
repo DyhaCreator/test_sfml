bd16407edc51af46eaaa076e339df230241904bc
DyhaCreator <andropet228@gmail.com>
2023-07-05 00:48:14 -0400

    add deleting *.h.gch

--- compiler.sh
+++ compiler.sh
@@ -1,6 +1,7 @@
 echo "compile"
-g++ -c main.cpp draw.h -I /SFML/include
+g++ -c main.cpp *.h -I /SFML/include
 g++ main.o -o sfml-app -L /SFML/lib -lsfml-graphics -lsfml-window -lsfml-system
+rm *.h.gch
 rm main.o
 echo "start>"
 ./sfml-app
